9ba10b9b3c5c3b4e858084bd604245d518df814a
Christopher Carson <chris@nowzoo.com>
2015-06-26 15:30:28 -0500

    this should make it clean

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ exec(cmd, function(err, stdout){
     var branch_line;
     var branches;
     var status = {};
+    var result;
     if (err) return callback(err);
     lines = stdout.split('\n');
     branch_line = lines.shift().replace(/\#\#\s+/, '');
+    console.log(stdout);
     branches = branch_line.split('...');
     status.local = branches[0];
+    status.remote_diff = null;
     if (branches[1]){
-        
+        result = branches[1].match(/^([^\s]+)/);
+        status.remote = result[1];
+        result = branches[1].match(/\[([^\]]+)\]/);
+        status.remote_diff = result ? result[1] : null;
     }
-    console.log(branches);
+    status.clean = lines.length === 0;
+    console.log(status);
 
 
     //callback(err, stdout.length > 0);
