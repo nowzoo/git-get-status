db91cc25afe5f35905972d9c5b4bf9dbbf6a5654
Christopher Carson <chris@nowzoo.com>
2015-06-26 14:56:18 -0500

    regex

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ var cmd = 'git status --porcelain -b';
 
 exec(cmd, function(err, stdout){
     var branches;
-    var branch_rx = /^\#\#\s+([^\n]+)/;
+    var branch_rx = /^\#\#\s+([^]+)(...([^\s]+)(\s\[([^\]]+))?)?/;
     var status;
     if (err) return callback(err);
     branches = stdout.match(branch_rx)[1].split('...');
-    status = {
-        clean: stdout.split('\n').length === 1,
-        local: branches[0],
-        remote: branches.length === 2 ? branches[1] : null
-    };
+    status = stdout.match(branch_rx);
     console.log(status);
 
 
